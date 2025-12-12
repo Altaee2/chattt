bba803726a12dec0647f037f5968bca2940886b1
Ali altaee <yl0206382@gmail.com>
2025-12-12 13:38:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -182,4 +182,4 @@ if __name__ == '__main__':
     
     # استخدام host='0.0.0.0' لتوفير الوصول عبر IP المحلي
     # استخدام debug=True يتيح إعادة التحميل التلقائي للكود
-    app.run(host='0.0.0.0', port=5000, debug=True)
+   serve(app, host='0.0.0.0', port=5000)
